c9296f24790664798ec5779558d41432029eeeee
Astryda Malinowska <astamalinowska@outlook.com>
2023-01-12 12:46:50 +0100

    created hello_world file with initial content

---
+++ 01_hello_world.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print("Hello, World!")
+print("My name is Astryda, and I'm refreshing my knowledge,")
+print("with Eric Matthes book \"Python Crash Course\".")
+print("Well... Wish me luck c:")
