6162af32f1c93ed4fbb792dc8e49dbf87dfeef50
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-07 10:47:30 +0100

    if construction

---
+++ 06_voting.py
@@ -0,0 +1,4 @@
+age = 19
+if age >= 18:
+    print("Wow, you can legally wote...")
+    print("But... smash dumbocracy, only Anarchy!")
---
+++ 07_voting.py
@@ -0,0 +1,7 @@
+age = 17
+if age >= 18:
+    print("Wow, you can legally vote...")
+    print("But smash this idiocracy, only Ancap!")
+else:
+    print("Oh, you cant legally vote, you are to young...")
+    print("But you can become an Anarchist!")
